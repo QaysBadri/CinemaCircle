17e59b8801e7dc07b2483c8520a42f1c524fb4f8
levalleyjack <levalleyjack@gmail.com>
2023-09-14 20:31:45 -0700

    backend and listener

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,12 +1,30 @@
 import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
+import axios from "axios";
 
 export default function TabOneScreen() {
+  const [message, setMessage] = useState("empty");
+
+  useEffect(() => {
+    console.log("Fetching data");
+    axios
+      .get("http://10.10.0.11:8080/hello")
+      .then((response) => {
+        console.log("Response:", response);
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>{message}</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
